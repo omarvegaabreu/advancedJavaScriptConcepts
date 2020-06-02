c0a2f8c85abec09e504b74d0eeb12657959007ce
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2020-06-02 07:02:15 -0400

    functions

--- functions/functions.js
+++ functions/functions.js
@@ -1,3 +1,56 @@
+var c = "c";
+function sayMyName() {
+  var a = "a";
+  return function findMyName() {
+    var b = "b";
+    console.log(c);
+    return function printName() {
+      var c = "c";
+      return "Omar Vega";
+    };
+  };
+}
+
+sayMyName()()();
+
+function findMyName() {
+  var b = "b"; //added to var env
+  return printMyName(); //3
+}
+
+function printMyName() {
+  var c = "c"; //added to var env
+
+  return "Omar Vega"; //4
+}
+
+function sayMyName() {
+  var a = "a"; //added to variable environment
+
+  return findMyName(); //2
+}
+
+// console.log(sayMyName());
+
+//sayMyName()
+//global
+//callStack
+
+/********************************* */
+
+function two() {
+  var isValid;
+}
+
+function one() {
+  var isValid = true;
+  two();
+}
+
+var isValid = false;
+// console.log(one());
+// console.log(isValid);
+
 //function expression
 //are defined at run time,
 //when the function is called or executed
@@ -22,11 +75,11 @@ function marry(person1, person2) {
   // wil convert An array-like
   // or iterable object to convert to an array.
   // you pass to it to an Array.
-  console.log(Array.from(arguments));
-  console.log(`${person1} is married to ${person2}`);
+  // console.log(Array.from(arguments));
+  // console.log(`${person1} is married to ${person2}`);
 }
 
-console.log(marry("omar", "rosely"));
+// console.log(marry("omar", "rosely"));
 
 // const name = "omar";
 // console.log(Array.from(name));
