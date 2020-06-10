26e8e246c14ead6ccae6e61fc2e23ba943ecbac4
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2020-06-10 16:27:28 -0400

    oop

--- OOP/oopBasics.js
+++ OOP/oopBasics.js
@@ -12,18 +12,78 @@
 /**
  * Object.create() allow us to "bind" a function to
  * a new object and/or create a different
+ *
+ * SUB-CLASSING when you use the key word
+ * EXTENDS. The sub-class in this case will
+ * inherit all the properties and methods from
+ * the parent class.
+ *
+ * INSTANCE is when we use the new key word of a class,
+ * or simply put when we clone a class or copy a class
+ *
+ *
+ * 4 PILLARS OF OOP
+ *
+ * ENCAPSULATION:Encapsulation is the packing of data and functions into one component
+ * (for example, a class) and then controlling access to that component to make a "blackbox"
+ *  out of the object. Because of this, a user of that class only needs to know its
+ * interface (that is, the data and functions exposed outside the class),
+ * not the hidden implementation.
+ *
+ * ABSTRACTION:Abstraction in computer programming is a way to reduce complexity
+ * and allow efficient design and implementation in complex software systems.
+ * It hides the technical complexity of systems behind simpler APIs.
+ *
+ * INHERITANCE : The PARENT CLASS AND SUBCLASS OR CHILD CLASS are linked
+ * to the parent thought the prototype chain. Also INHERITANCE helps SHARE MEMORY
+ * SPACE
+ *
+ * POLYMORPHISM: THE WORD IT SELF MEANS MANY FORMS. Its the ability to call the same
+ * method on different objects and each object responding in a different way. It has
+ * the ability to process objects differently depending on their data types
+ * or class.
+ *
+ * BENEFITS OF OOP:
+ *
+ * CLEAR AND UNDERSTANDABLE CODE.
+ * EASY TO EXTEND.
+ * EASY TO MAINTAIN
+ * MEMORY EFFICIENT
+ * HELPS KEEP CODE DRY
+ *
  */
-const elfAttackStore = {
+class Character {
+  constructor(name, race, weapon, attack, special) {
+    this.name = name;
+    this.race = race;
+    this.weapon = weapon;
+    this.attack = attack;
+    this.special = special;
+  }
+  greeting() {
+    return `My name is ${this.name}, I am a ${this.race}`;
+  }
   attack() {
-    return `Attack with ${this.weapon}`;
-  },
-};
-function createElf(name, weapon) {
-  let elf = Object.create(elfAttackStore);
-  elf.name = name;
-  elf.weapon = weapon;
-  return elf;
+    return `${this.attack}`;
+  }
 }
-const omar = createElf("Omar", "club");
 
-console.log(omar.attack());
+class Elf extends Character {
+  constructor(name, race, weapon, attack, special, attackPower) {
+    super(name, race, weapon, attack, special);
+    this.attackPower = attackPower;
+  }
+}
+
+class Oger extends Character {
+  constructor(name, race, weapon, attack) {
+    super(name, race, weapon);
+    this.attack = attack;
+  }
+}
+
+const jamesElf = new Elf("james", "elf", "word", "kick you", "nothing", 100);
+console.log(jamesElf);
+
+const jamesOger = new Oger("james oger", "oger fool", "none", "punch");
+console.log(jamesOger);
---
+++ OOP/oopExercise.js
@@ -0,0 +1,28 @@
+class Character {
+  constructor(name, weapon) {
+    this.name = name;
+    this.weapon = weapon;
+  }
+  attack() {
+    return "atack with " + this.weapon;
+  }
+}
+//Polymorphism--
+class Queen extends Character {
+  constructor(name, weapon, type = []) {
+    super(name, weapon);
+    this.type = type;
+  }
+  attack() {
+    return `I am vitoria the queen of ${this.type[0]}, now bow down to me!`;
+  }
+}
+//Extend the Character class to have a Queen class. The output of the below code should be:
+const victoria = new Queen("Victoria", "army", [
+  "hearts",
+  "clubs",
+  "spades",
+  "diamonds",
+]); // create a new instace with the queen having (name, weapon, type). Type inlcudes: 'hearts', 'clubs', 'spades', 'diamonds'
+
+victoria.attack(); // will console.log the attack() method in Character class AND will return another string: 'I am the Victoria of hearts, now bow down to me! '
