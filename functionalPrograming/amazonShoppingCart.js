e1dcdfb1ed294141272809ebee4eadb5a253d701
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2020-06-18 20:21:48 -0400

    compose and pipe

---
+++ functionalPrograming/amazonShoppingCart.js
@@ -0,0 +1,29 @@
+const user = {
+  name: "Omar",
+  active: true,
+  cart: [],
+  purchases: [],
+};
+
+function purchase() {}
+//1.add items to cart
+function addItem() {}
+//2.add 3% tax to item in the cart
+function addTax() {}
+//3.add items to purchase
+function addItemToPurchase() {}
+//4. empty cart
+function emptyCart() {}
+//5.accept refunds
+function refund() {}
+//6.track history
+function history() {}
+
+//check if there is an item
+//check if person wants to purchase
+//if the person wants to purchase
+//add the item to the cart
+//once the item is added to the purchase
+//add tax to it
+//track all items and tax in cart
+// make sure to accept refunds
